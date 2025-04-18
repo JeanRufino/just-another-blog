98f5ed7a43c64ac9d14f47b4029dc19241e6d3e6
Jean Rufino <jeanbruno1991@gmail.com>
2025-04-17 21:23:02 -0300

    navbar basics

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/navbar"
 
 const App = () => {
   return (
-    <div className='px-4 md:px-8 lg:px-16 lx:px-32 2lx:px-64'>
+    <div className='px-4 md:px-8 lg:px-16 lx:px-32 2lx:px-64 max-w-[100vw]'>
       <Navbar />
     </div>
   )
