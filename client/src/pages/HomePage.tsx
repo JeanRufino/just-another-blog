ca7a2a3e48e311b6cd70bfeb5a7601451f5a4414
JeanRufino <contatojeanrufino@gmail.com>
2026-04-04 00:32:44 -0300

    re

---
+++ client/src/pages/HomePage.tsx
@@ -0,0 +1,7 @@
+export default function HomePage() {
+  return (
+    <div className="h-screen w-full bg-gradient-to-b from-black to-[#021a0a] flex items-center justify-center">
+      <span className="text-white text-8xl font-bold select-none">&lt;/&gt;</span>
+    </div>
+  )
+}
